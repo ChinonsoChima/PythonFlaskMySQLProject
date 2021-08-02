5cf8736974205110c76a1869e55f289d00ed77e3
Chinonso Anyanka <cc792@njit.edu>
2021-08-02 10:42:31 -0400

    Added app config from tutorial

--- app/app.py
+++ app/app.py
@@ -1,19 +1,21 @@
-from typing import List, Dict
 import simplejson as json
-from flask import Flask, request, Response, redirect
-from flask import render_template
+from flask import Flask, request, Response, redirect, make_response
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from flask import render_template
+from forms import ContactForm
+
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 mysql = MySQL(cursorclass=DictCursor)
 
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'citiesData'
-mysql.init_app(app)
+#app.config['MYSQL_DATABASE_HOST'] = 'db'
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+#app.config['MYSQL_DATABASE_PORT'] = 3306
+#app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+#mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
@@ -115,6 +117,29 @@ def api_delete(city_id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect("/", code=302)
+    return render_template("contact.html", form=form)
+
+@app.errorhandler(404)
+def not_found():
+    """Page not found."""
+    return make_response(
+        'SORRY. THIS PAGE IS NOT FOUND.',
+        404
+     )
+
+
+@app.errorhandler(400)
+def bad_request():
+    """Bad request."""
+    return make_response(
+        'BAD REQUEST! THIS SERVER DOES NOT SUPPORT YOUR REQUEST.',
+        400
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
