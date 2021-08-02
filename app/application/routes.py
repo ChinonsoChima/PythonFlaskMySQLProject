b8656f8f5bda318d941a53f1e3ba7662492dab46
Chinonso Anyanka <cc792@njit.edu>
2021-08-02 11:06:36 -0400

    Added flask app factory from tutorial

---
+++ app/application/routes.py
@@ -0,0 +1,142 @@
+import simplejson as json
+from flask import Flask, request, Response, redirect, make_response
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+from flask import render_template
+from forms import ContactForm
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+mysql = MySQL(cursorclass=DictCursor)
+
+#app.config['MYSQL_DATABASE_HOST'] = 'db'
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+#app.config['MYSQL_DATABASE_PORT'] = 3306
+#app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+#mysql.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': 'Chino'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, cities=result)
+
+
+@app.route('/view/<int:city_id>', methods=['GET'])
+def record_view(city_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', city=result[0])
+
+
+@app.route('/edit/<int:city_id>', methods=['GET'])
+def form_edit_get(city_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', city=result[0])
+
+
+@app.route('/edit/<int:city_id>', methods=['POST'])
+def form_update_post(city_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
+    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
+    %s, t.fldAbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+@app.route('/cities/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New City Form')
+
+
+@app.route('/cities/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
+    sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+@app.route('/delete/<int:city_id>', methods=['POST'])
+def form_delete_post(city_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblCitiesImport WHERE id = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/cities', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/cities/<int:city_id>', methods=['GET'])
+def api_retrieve(city_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/cities/', methods=['POST'])
+def api_add() -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
+def api_edit(city_id) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/cities/<int:city_id>', methods=['DELETE'])
+def api_delete(city_id) -> str:
+    resp = Response(status=210, mimetype='application/json')
+    return resp
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect("/", code=302)
+    return render_template("contact.html", form=form)
+
+@app.errorhandler(404)
+def not_found():
+    """Page not found."""
+    return make_response(
+        'SORRY. THIS PAGE IS NOT FOUND.',
+        404
+     )
+
+
+@app.errorhandler(400)
+def bad_request():
+    """Bad request."""
+    return make_response(
+        'BAD REQUEST! THIS SERVER DOES NOT SUPPORT YOUR REQUEST.',
+        400
+    )
